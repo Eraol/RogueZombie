181b5c9572676306405f447cd6c53aa6d32586bb
Guillaume <guillaume.renet@gmail.Com>
2015-03-17 12:31:26 +0100

    Amélioration de l'affichage, bug secondes reglés.

--- app/src/main/java/fr/iutlens/roguezombie/Accueil.java
+++ app/src/main/java/fr/iutlens/roguezombie/Accueil.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
@@ -14,6 +16,8 @@ public class Accueil extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
